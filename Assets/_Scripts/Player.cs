e810bf12935086eea9ff3b53464a1f2e191b4142
Paul Davis <paulcdavis135@gmail.com>
2023-06-27 00:53:10 -0400

    Basic movement implementation using a CharacterController component
    demo: https://github-production-user-asset-6210df.s3.amazonaws.com/59056905/249039594-cf1a4435-0205-4744-9bfa-765f62f93c0d.mp4

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,41 +1,100 @@
-using Cinemachine.Utility;
+using Mono.Cecil.Cil;
 using UnityEngine;
-using UnityEngine.InputSystem;
-
 using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Transform orientation;
+    //[SerializeField] Transform orientation;
+    [SerializeField] Transform followCamera;
 
-    private PlayerInputActions inputActions;
-    private Rigidbody rb;
+    private CharacterController playerController;
 
     // should have this in a ScriptableObject
-    private const float speed = 5f;
+    private const float playerSpeed = 5f;
+    private const float gravity = -9.81f;
+    private float verticalVelocity;
+    private bool isGrounded;
 
-    // Start is called before the first frame update
-    void Awake()
+    private float verticalInput = 0f;
+    private float horizontalInput = 0f;
+    private float playerHeight;
+    private int jumps = 0;
+
+    // passive movement fields
+    [Header("Movement Fields")]
+    [SerializeField] private float gravityMultiplier = 1f;
+    [SerializeField] private Vector3 playerDirection;
+    [SerializeField] private float verticalTerminalVelocity = 120f;
+
+
+    // Awake is called before the first frame update
+    private void Awake()
     {
-        inputActions = new PlayerInputActions();
-        rb = GetComponent<Rigidbody>();
+        playerController = GetComponent<CharacterController>();
+
+        playerHeight = playerController.height;
+    }
+
+    private void Update()
+    {
+        GroundedCheck();
+        UpdateMove();
+
+        // handle gravity
+        if (verticalVelocity < verticalTerminalVelocity)
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+    }
+
+    private void UpdateMove()
+    {
+        verticalInput = playerDirection.y;
+        horizontalInput = playerDirection.x;
+
+        // Calculate movement relative to camera
+        Vector3 cameraForward = Vector3.ProjectOnPlane(followCamera.forward, Vector3.up).normalized;
+        Vector3 cameraRight = Vector3.ProjectOnPlane(followCamera.right, Vector3.up).normalized;
+        Vector3 cameraOrientedPlayerDirection = (cameraForward * verticalInput + cameraRight * horizontalInput);
+
+        playerController.Move(cameraOrientedPlayerDirection * playerSpeed * Time.deltaTime + Vector3.up * verticalVelocity * Time.deltaTime);
+    }
+
+    private void GroundedCheck()
+    {
+        LayerMask groundLayer = 1 << 6 | 1 << 7;
+        RaycastHit hit;
+
+        float raycastDistance = playerHeight / 2 + 0.1f;
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, groundLayer);
+
+        if (isGrounded)
+        {
+            jumps = 0;
+        }
     }
 
     public void Jump(CallbackContext context)
     {
-        if (context.performed)
+        if (isGrounded || jumps < 2)
         {
-            rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
+            verticalVelocity = Mathf.Sqrt(-(2f * gravity * 1f));
+            jumps += 1;
         }
     }
 
     public void Move(CallbackContext context)
     {
-        if (context.performed)
+        // move in direction of orientation
+        if (context.performed || context.canceled)
         {
-            Vector2 direction = context.ReadValue<Vector2>().normalized;
-            Vector3 move = (orientation.forward * direction.y * speed + orientation.right * direction.x * speed);
-            rb.AddForce(move, ForceMode.Impulse);
+            playerDirection = context.ReadValue<Vector2>();
         }
     }
 }
